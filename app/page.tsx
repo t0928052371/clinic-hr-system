c9bc17ccf6dc2e11a85953416995232a599023eb
Perry <t0928052371@gmail.com>
2026-05-05 18:49:46 +0800

    fix home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>診所 HR 管理系統</h1>
-      <p>第一版系統已啟動 🚀</p>
-    </main>
-  );
+  return <div>Home OK</div>;
 }
